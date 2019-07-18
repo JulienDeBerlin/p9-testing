726c49c0dca4a3f7ced3aa2c33ee2112c26e2993
JulienDeBerlin <julien_berthoud@yahoo.fr>
2019-07-18 14:06:49 +0200

    Correction bug in getByNumero() in Class CompteComptable : null param must return null

--- myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
@@ -11,31 +11,29 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CompteComptableTest {
 
 
-
     @Test
     public void getByNumeroTest() {
 
-        Integer account1 = 1232;
-        Integer account2 = 92837;
-        Integer account3 = null;
+        Integer numero1 = 1232;
+        Integer numero2 = 92837;
+        Integer numero3 = null;
 
-        String name1 = "apports en capital";
-        String name2 = null;
-        String name3 = "produits exceptionnels";
+        String libellé1 = "apports en capital";
+        String libellé2 = null;
+        String libellé3 = "produits exceptionnels";
 
-        CompteComptable compteComptable1 = new CompteComptable(account1, name1);
-        CompteComptable compteComptable2 = new CompteComptable(account2, name2);
-        CompteComptable compteComptable3 = new CompteComptable(account3, name3);
-
+        CompteComptable compteComptable1 = new CompteComptable(numero1, libellé1);
+        CompteComptable compteComptable2 = new CompteComptable(numero2, libellé2);
+        CompteComptable compteComptable3 = new CompteComptable(numero3, libellé3);
 
         List<CompteComptable> list3elements = new ArrayList<CompteComptable>(
                 Arrays.asList(compteComptable1, compteComptable2, compteComptable3));
 
-        assertEquals(compteComptable1, CompteComptable.getByNumero(list3elements, account1), "3 elements, standard");
-        assertEquals(compteComptable2, CompteComptable.getByNumero(list3elements, account2), "3 elements, name is null");
+        assertEquals(compteComptable1, CompteComptable.getByNumero(list3elements, numero1), "3 elements, standard");
+        assertEquals(compteComptable2, CompteComptable.getByNumero(list3elements, numero2), "3 elements, name is null");
 
         //FIXME: ça ne devrait pas être possible de passer null en paramètre
-        assertEquals(null, CompteComptable.getByNumero(list3elements, account3), "3 elements, account is null");
+        assertEquals(null, CompteComptable.getByNumero(list3elements, numero3), "3 elements, account is null");
         assertEquals(null, CompteComptable.getByNumero(list3elements, 5555), "3 elements, account not available");
 
 
