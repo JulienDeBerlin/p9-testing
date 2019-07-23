9cd5e3b605f4cf087496e5a167a00b5dac447bdd
JulienDeBerlin <julien_berthoud@yahoo.fr>
2019-07-23 11:23:49 +0200

    Migration to Spring5 + fix dependency conflict

--- myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/TestComptabiliteManagerImpl.java
+++ myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/TestComptabiliteManagerImpl.java
@@ -14,6 +14,7 @@ import org.mockito.Mockito;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
